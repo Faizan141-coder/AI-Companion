0e69acfbcccd275cc0460d268370df37132f3f1d
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-11-25 18:30:42 +0500

    Companion Creation API

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import axios from 'axios';
 import * as z from 'zod';
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
@@ -27,6 +28,8 @@ import {
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
+import { useRouter } from 'next/navigation';
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -76,6 +79,9 @@ export const CompanionForm = ({
     intialData
 }: CompanionFormProps) => {
 
+    const { toast } = useToast()
+    const router = useRouter()
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: intialData || {
@@ -91,7 +97,27 @@ export const CompanionForm = ({
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            if (intialData) {
+                // Update Companion functionality
+                await axios.patch(`/api/companion/${intialData.id}`, values)
+            } else {
+                // Create Companion functionality
+                await axios.post('/api/companion', values)
+            }
+
+            toast({
+                description: 'Success',
+            })
+
+            router.refresh()
+            router.push('/')
+        } catch (error) {
+            toast({
+                variant: 'destructive',
+                description: 'Something went wrong',
+            })
+        }
     }
 
     return (
