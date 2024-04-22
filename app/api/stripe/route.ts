a4d49d380f0f0f34cbec715f3c97e35946313ca0
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-04-23 01:31:25 +0500

    stripe bug fixed

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -3,17 +3,16 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
-import { abslouteUrl } from "@/lib/utils";
-import { url } from "inspector";
+import { absoluteUrl } from "@/lib/utils";
 
-const settingsUrl = abslouteUrl("/settings");
+const settingsUrl = absoluteUrl("/settings");
 
 export async function GET() {
   try {
     const { userId } = auth();
     const user = await currentUser();
 
-    if (!user || !userId) {
+    if (!userId || !user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
@@ -33,37 +32,36 @@ export async function GET() {
     }
 
     const stripeSession = await stripe.checkout.sessions.create({
-        success_url: settingsUrl,
-        cancel_url: settingsUrl,
-        payment_method_types: ["card"],
-        mode: "subscription",
-        billing_address_collection: "auto",
-        customer_email: user.emailAddresses[0].emailAddress,
-        line_items: [
-            {
-                price_data: {
-                    currency: "USD",
-                    product_data: {
-                        name: "Companion Pro",
-                        description: "Create Custom AI Companions",
-                    },
-                    unit_amount: 999,
-                    recurring: {
-                        interval: "month",
-                    }
-                },
-                quantity: 1,
-            }
-        ],
-        metadata: {
-            userId
-        }
-    })
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Companion Pro",
+              description: "Create Custom AI Companions",
+            },
+            unit_amount: 999,
+            recurring: {
+              interval: "month",
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    });
 
-    return new NextResponse(JSON.stringify({ url: stripeSession.url }))
-
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
   } catch (error) {
-    console.log("[STRIPE_GET]", error);
+    console.log("[STRIPE]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,7 +6,7 @@ import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
 
 export async function POST(req: Request) {
-  const body = await req.json();
+  const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
 
   let event: Stripe.Event;
